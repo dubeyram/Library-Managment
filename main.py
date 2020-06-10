f099432c9a3f39787aa95872c03f13550093370f
Ram Gopal Dubey <61063171+dubeyram@users.noreply.github.com>
2020-06-11 01:55:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ class Library:
             print("Lender-Book database has been updated. You can take the book now")
         else:
             print(f"Book is already being used by {self.lenddict[book]}")
+    
+    def searchbook(self , book):
+        if book not in self.booksList:
+            print("This book is not avaliable.")
+            print("You want to add this book: 'Y' or 'N' ")
+            c = input()
+            if c=="Y":
+                book = input("Enter the book name:")
+                ram.addbook(book)
+            else:
+                pass
 
     def addbook(self , book):
         self.booksList.append(book)
@@ -24,15 +35,17 @@ class Library:
     def returnbook(self,book):
         self.lenddict.pop(book)
 if __name__=="__main__":
-    ram = Library(['Python','Rich Daddy Poor Daddy','Harry Potter','C++ Basics'],"RamGopal")
+    ram = Library(['Python','Rich Daddy Poor Daddy','Harry Potter','C++ Basics'],"World")
 
     while(True):
 
-        print(f"Welcome to the {ram.name} Library. Enter your choice to continue: ")
+        print(f"Welcome to the {ram.name} Library. \n Enter your choice to continue: ")
         print("1. Display Books") 
         print("2. Lend a Book")
         print("3. Add a Book")
         print("4. Return a Book")
+        print("5. Enter the book to Search")
+
         user_choice = int(input())
 
         if user_choice == 1:
@@ -47,10 +60,13 @@ if __name__=="__main__":
         elif user_choice ==4:
             book= input("Enter the book you want to return")
             ram.returnbook(book)
+        elif user_choice==5:
+            book = input("Enter the book to search")
+            ram.searchbook(book)
         else:
             print("Not a valid choice")
         
-        print("Press q to Quit aand c to continue")
+        print("Press q to Quit and c to continue")
         user_choice2=""
         while(user_choice2!="c" and user_choice2!="q"):
             user_choice2  = (input("Enter your choice:"))
@@ -58,6 +74,3 @@ if __name__=="__main__":
                 exit()
             elif user_choice2=="c":
                 continue
-             
-        
-        
